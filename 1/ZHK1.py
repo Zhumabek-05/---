04e7734bf655e76d35039f7db00a10a75981d376
Zhumabek-05 <kanatbaevzumabek@gmail.com>
2025-09-23 16:45:51 +0500

    Add files via upload
    Канатбаев Жумабек

---
+++ 1/ZHK1.py
@@ -0,0 +1,12 @@
+a = 10
+b = 3
+
+print(a - b)  # азайту
+print(a + b)  # косу
+print(a * b)  # кобейту
+print(a ** b) # дареже
+print(a / b)  # болу
+print(a // b) # бутын санд болу
+print(a % b)  # остаток
+
+print()
